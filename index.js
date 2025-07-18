97446c068eb7ed77524e263572e940601d18b9d9
M3Coding <meady2009@gmail.com>
2025-07-17 22:00:53 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const port = 3000;
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
